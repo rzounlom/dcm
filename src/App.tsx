12110531e29a1e96809a54831a4c8a3e2fea82bc
rzounlom <rzounlom@gmail.com>
2025-08-12 19:30:59 -0400

    complete phase 1 of dream car tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
+import CarList from "./components/cars/CarList";
+import Landing from "./components/landing/Landing";
+import Navbar from "./components/navbar/Navbar";
+import { cars } from "./data/car";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div className="page-container">
+      <Navbar />
+      <Landing />
+      <CarList cars={cars} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Props.tsx
@@ -0,0 +1,4 @@
+export const Props = ({ count }) => {
+  //   console.log(props);
+  return <div>the current count is {count}</div>;
+};
