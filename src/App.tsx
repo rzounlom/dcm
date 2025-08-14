62b400b87f60f11f56f9d2e435b72856b0fd02ab
rzounlom <rzounlom@gmail.com>
2025-08-13 20:47:25 -0400

    move cars to state

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import CarList from "./components/cars/CarList";
 import Landing from "./components/landing/Landing";
 import Navbar from "./components/navbar/Navbar";
 import { cars } from "./data/car";
+import { useState } from "react";
 
 function App() {
+  const [carsState, setCarsState] = useState(cars);
+
   return (
     <div className="page-container">
       <Navbar />
       <Landing />
-      <CarList cars={cars} />
+      <CarList cars={carsState} />
     </div>
   );
 }
